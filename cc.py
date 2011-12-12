8b13401d516fef5f794853fa1d6f066b73173926
Christine Tölzer <chtoelzer@yahoo.de>
2011-12-12 17:32:50 +0100

    Hallo Welt

---
+++ cc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""USAGE:
+"""
+
+__version__ = 0.03
+__author__ = "Christine Tölzer"
+
+""" Naechste Schritte:
+in Webseite mit iframe einbinden
+"""
+
+print("Hallo Welt")
